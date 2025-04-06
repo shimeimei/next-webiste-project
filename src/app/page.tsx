86198891c6fd78a4d67ddf51e4df27449002e76d
史女士 <345210068@qq.com>
2025-04-06 17:14:43 +0800

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
-import Image from "next/image";
+// import Image from "next/image";
 type ButtonProps = {
-  name?: string
-}
+  name?: string;
+};
 export default function Home() {
   return (
     <div>
       <h1>Welcome to my app</h1>
-      <MyButton name='aa'/>
+      <MyButton name="aa" />
     </div>
   );
 }
-function MyButton({name='button'}: ButtonProps) {
-  return <button className="bg-sky-500 hover:bg-sky-700 w-300">{name }</button>;
+function MyButton({ name = "button" }: ButtonProps) {
+  return <button className="bg-sky-500 hover:bg-sky-700 w-300">{name}</button>;
 }
